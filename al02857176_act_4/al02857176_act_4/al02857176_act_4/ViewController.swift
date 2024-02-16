fce2912eaf9c152b4f344641d8ab04ca6912998a
Riquelme-Carlos <carlosriquelmedeveloper@gmail.com>
2024-02-15 21:13:00 -0600

    Agregando actividad a repo

---
+++ al02857176_act_4/al02857176_act_4/al02857176_act_4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  al02857176_act_4
+//
+//  Created by Alumno on 14/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+// Carlos Alberto Riquelme Valdivia Actividad 4 - 2857176
+    
+}
+
---
+++ al02857176_act_4/al02857176_act_4/al02857176_act_4UITests/al02857176_act_4UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  al02857176_act_4UITestsLaunchTests.swift
+//  al02857176_act_4UITests
+//
+//  Created by Alumno on 14/02/24.
+//
+
+import XCTest
+
+final class al02857176_act_4UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
